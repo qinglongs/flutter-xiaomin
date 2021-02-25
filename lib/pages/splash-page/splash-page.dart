c27527867bc6881514861d40b4e9cfbe859c0ebc
lqllm <924229679@qq.com>
2021-02-25 14:06:56 +0800

    feat(闪屏页)：闪屏页定义

---
+++ lib/pages/splash-page/splash-page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text('老铁这是启动页'),
+    );
+  }
+}
