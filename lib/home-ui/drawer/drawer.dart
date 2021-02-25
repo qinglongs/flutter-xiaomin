3a31cff5bef499be335795cba6285d4f6d8204d2
lqllm <924229679@qq.com>
2021-02-25 14:06:35 +0800

    feat(抽屉组件)：抽屉逻辑完善

--- lib/home-ui/drawer/drawer.dart
+++ lib/home-ui/drawer/drawer.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'dart:ui';
 
-class MyDrawer extends StatelessWidget {
+class MyDrawer extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyDrawerState();
+  }
+}
+
+class MyDrawerState extends State<MyDrawer> {
+  int _num = 0;
   @override
   Widget build(BuildContext context) {
+    // 3
     return Container(
         margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
         padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
@@ -15,209 +23,66 @@ class MyDrawer extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                SizedBox(
-                  height: 5,
-                ),
+                Text('$_num'),
                 Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  height: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.lightBlue,
-                      border: Border(bottom: BorderSide(color: Colors.black))),
-                ),
+                  width: double.infinity,
+                  child: IconButton(
+                      icon: Text('请点我'),
+                      onPressed: () {
+                        setState(() {
+                          _num = _num + 1;
+                        });
+                      }),
+                )
               ],
             ),
           ),
         ));
   }
+
+  @override
+  void initState() {
+    // 1
+    super.initState();
+    print('initState');
+  }
+
+  @override
+  void didUpdateWidget(MyDrawer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("didUpdateWidget");
+  }
+
+  @override
+  void deactivate() {
+    // 销毁 1
+    super.deactivate();
+    print("deactive");
+  }
+
+  @override
+  void dispose() {
+    // 销毁 2
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    print("reassemble");
+  }
+
+  @override
+  void didChangeDependencies() {
+    // 2
+    super.didChangeDependencies();
+    print("didChangeDependencies");
+  }
 }
+
+// 初始化  initState didChangeDependencies build
+
+// setState build
+
+// 离开页面 deactivate dispose
